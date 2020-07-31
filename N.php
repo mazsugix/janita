882bed825b81805edde3acd79796a0c82fbc56e3
mazsugix <68642543+mazsugix@users.noreply.github.com>
2020-08-01 06:30:50 +0700

    Update N.php

--- N.php
+++ N.php
@@ -9,7 +9,7 @@ $header[] = "X-AppVersion: 3.46.1";
 $header[] = "X-UniqueId: ".time()."57".mt_rand(1000,9999);
 $header[] = "Connection: keep-alive";
 $header[] = "X-User-Locale: id_ID";
-$header[] = "X-Location: -8.67".mt_rand(1000,9999).",115.21".mt_rand(1000,9999);
+$header[] = "X-Location: -5.152".mt_rand(1000,9999).",119.431".mt_rand(1000,9999);
 if ($pin):
 $header[] = "pin: $pin";
     endif;
