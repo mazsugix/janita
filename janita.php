99a691aa4b98d48a697c13717429908b5e554244
mazsugix <68642543+mazsugix@users.noreply.github.com>
2020-08-01 06:28:59 +0700

    Update janita.php

--- janita.php
+++ janita.php
@@ -93,7 +93,7 @@ ulang:
         sleep(5);
         }
         sleep(3);
-        $boba09 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
+        $boba09 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"MAUGOFOOD2107"}');
         $messageboba09 = fetch_value($boba09,'"message":"','"');
         echo "\n".color("white"," Message: ".$messageboba09);
         sleep(1);
