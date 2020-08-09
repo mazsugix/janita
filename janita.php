7b051dcbde9b970284c61f1c7c93571750a2633a
mazsugix <68642543+mazsugix@users.noreply.github.com>
2020-08-09 13:36:52 +0700

    Update janita.php

--- janita.php
+++ janita.php
@@ -69,7 +69,7 @@ ulang:
         echo color("white",".");
         sleep(10);
         }
-        $code1 = request1('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD2107"}');
+        $code1 = request1('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD0508"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'sukses')){
         echo "\n".color("green","Message: ".$message);
@@ -83,7 +83,7 @@ ulang:
         echo color("white",".");
         sleep(5);
         }
-        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"PESANGOFOOD2107"}');
+        $code1 = request2('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD0508"}');
         $message = fetch_value($code1,'"message":"','"');
         echo "\n".color("white"," Message: ".$message);
         echo "\n".color("blue"," 🔹Voucher4.");
